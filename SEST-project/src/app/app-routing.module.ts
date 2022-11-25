bb3f48516f8ebdd77a188c5c12fcdc20fdc6e303
Otremba <laysa.santos919@gmail.com>
2022-11-25 00:00:45 -0300

    paginas de videos liberada pra validação sem responsivo

--- SEST-project/src/app/app-routing.module.ts
+++ SEST-project/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { VideosComponent } from './pages/videos/videos.component';
+
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'videos', component: VideosComponent },
 ];
 
 @NgModule({

a2db488506135c00607b8b82ec87e2b4cc7a803c
Laysa Santana <laysa.santos919@gmail.com>
2022-12-05 12:28:10 -0300

    ultima etapa - modal videos

--- SEST-project/src/app/pages/videos/videos.component.ts
+++ SEST-project/src/app/pages/videos/videos.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
   styleUrls: ['./videos.component.scss']
 })
 export class VideosComponent {
+
+  closeResult: string = '';
+
+  constructor(private modalService: NgbModal) { }
+
+
+  open(content: any, size:any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' ,  size: size}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
 }
